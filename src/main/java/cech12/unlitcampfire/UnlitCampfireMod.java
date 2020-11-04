504e2a352e62b69caaa99a0e5ef78550ed605438
Christian Voß <cech12@gmail.com>
2020-11-04 10:37:16 +0100

    add forge

---
+++ src/main/java/cech12/unlitcampfire/UnlitCampfireMod.java
@@ -0,0 +1,8 @@
+package cech12.unlitcampfire;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("unlitcampfire")
+public class UnlitCampfireMod {
+    public static final String MOD_ID = "unlitcampfire";
+}
