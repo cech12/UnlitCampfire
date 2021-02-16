2da62e4a1e4a8e7e04f1ae064298ac296aece3b4
JayCeeCreates <gagaring.cedric@gmail.com>
2021-02-17 00:31:52 +0800

    Ported to Fabric 1.16

--- src/main/java/cech12/unlitcampfire/UnlitCampfireMod.java
+++ src/main/java/cech12/unlitcampfire/UnlitCampfireMod.java
@@ -1,20 +1,20 @@
 package cech12.unlitcampfire;
 
-import cech12.unlitcampfire.config.ServerConfig;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.loading.FMLConfig;
-import net.minecraftforge.fml.loading.FMLPaths;
+import cech12.unlitcampfire.config.UnlitCampfireConfig;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
+import net.fabricmc.api.ModInitializer;
 
-@Mod("unlitcampfire")
-public class UnlitCampfireMod {
+public class UnlitCampfireMod implements ModInitializer {
+
+    public static UnlitCampfireConfig CONFIG;
     public static final String MOD_ID = "unlitcampfire";
 
-    public UnlitCampfireMod() {
+    @Override
+    public void onInitialize() {
         //Config
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ServerConfig.SERVER_CONFIG);
-        ServerConfig.loadConfig(ServerConfig.SERVER_CONFIG, FMLPaths.GAMEDIR.get().resolve(FMLConfig.defaultConfigPath()).resolve(MOD_ID + "-server.toml"));
+        AutoConfig.register(UnlitCampfireConfig.class, GsonConfigSerializer::new);
+        CONFIG = AutoConfig.getConfigHolder(UnlitCampfireConfig.class).getConfig();
     }
 
 }
